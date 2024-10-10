9810aa02939f0536cb6debe595ba25c6054b97ff
MatiasAlvealC <matiasalvealc@gmail.com>
2024-10-10 15:29:30 -0300

    Agregado Proyecto con conexion BD

---
+++ ProyectoBD/gallery/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Galeria
+
+def galeria(request):
+    galerias = Galeria.objects.all()
+    return render(request,"gallery/galeria.html", {'galerias':galerias})
