8bf4e211d9076e6a19bd61685d8d5c2392063e74
MatiasAlvealC <matiasalvealc@gmail.com>
2024-10-17 14:48:20 -0300

    Mejorado proyectoBD

--- ProyectoBD/gallery/views.py
+++ ProyectoBD/gallery/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Galeria
+from django.core.paginator import Paginator
 
 def galeria(request):
-    galerias = Galeria.objects.all()
-    return render(request,"gallery/galeria.html", {'galerias':galerias})
+  galerias = Galeria.objects.all()
+  paginator = Paginator(galerias, 3)  # Show 3 galeries per page.
+  page_number = request.GET.get("page")
+  page_obj = paginator.get_page(page_number)
+  return render(request, "gallery/galeria.html", {"page_obj": page_obj})
