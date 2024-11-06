2130038c0825e733bdfc9f252c877e94558f8bc9
sebastian <sebastian.galindo05@inacapmail.cl>
2024-11-06 12:21:27 -0300

    agregado de peliculas

--- lasuciacritica/criticas/views.py
+++ lasuciacritica/criticas/views.py
@@ -5,35 +5,35 @@ from django.db.models import Q
 
 def home(request):
   peliculas = Pelicula.objects.all()
-  paginator = Paginator(peliculas, 12)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request, "criticas/home.html", {"page_obj": page_obj})
 
 def accion_aventura(request):
   peliculas = Pelicula.objects.filter(Q(genero__icontains="Animación") | Q(genero__icontains="Acción"))
-  paginator = Paginator(peliculas, 9)  
+  paginator = Paginator(peliculas, 30)  
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)  
   return render(request, "criticas/accion_aventura.html", {"page_obj": page_obj})
 
 def comedia(request):
   peliculas = Pelicula.objects.filter(genero__icontains="Comedia")
-  paginator = Paginator(peliculas, 9)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request,"criticas/comedia.html", {"page_obj": page_obj})
 
 def drama(request):
   peliculas = Pelicula.objects.filter(genero__icontains="Drama")
-  paginator = Paginator(peliculas, 9)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request,"criticas/drama.html", {"page_obj": page_obj})
 
 def ciencia_fantasia(request):
   peliculas = Pelicula.objects.filter(Q(genero__icontains="Ciencia Ficción") | Q(genero__icontains="Fantasía"))
-  paginator = Paginator(peliculas, 9)  
+  paginator = Paginator(peliculas, 30)  
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)  
   return render(request, "criticas/ciencia_fantasia.html", {"page_obj": page_obj})
@@ -42,28 +42,28 @@ def ciencia_fantasia(request):
 
 def horror_suspenso(request):
   peliculas = Pelicula.objects.filter(Q(genero__icontains="Terror") | Q(genero__icontains="Suspenso"))
-  paginator = Paginator(peliculas, 9)  
+  paginator = Paginator(peliculas, 30)  
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)  
   return render(request,"criticas/horror_suspenso.html", {"page_obj": page_obj})
 
 def romance(request):
   peliculas = Pelicula.objects.filter(genero__icontains="Romance")
-  paginator = Paginator(peliculas, 9)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request,"criticas/romance.html", {"page_obj": page_obj})
 
 def animacion(request):
   peliculas = Pelicula.objects.filter(genero__icontains="Animación")
-  paginator = Paginator(peliculas, 9)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request, "criticas/animacion.html", {"page_obj": page_obj})
 
 def documental(request):
   peliculas = Pelicula.objects.filter(genero__icontains="Documental")
-  paginator = Paginator(peliculas, 9)  # Show 3 galeries per page.
+  paginator = Paginator(peliculas, 30)  # Show 3 galeries per page.
   page_number = request.GET.get("page")
   page_obj = paginator.get_page(page_number)
   return render(request,"criticas/documental.html", {"page_obj": page_obj})
